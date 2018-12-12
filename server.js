75e043bb7446be255b71a96ae69a8b7ec683fe5a
Jenna Murphy <jenna.raye.murphy@gmail.com>
2018-12-12 08:56:53 -0800

    run on npm start

---
+++ server.js
@@ -0,0 +1,6 @@
+var connect = require('connect');
+var serveStatic = require('serve-static');
+let port = 3000;
+connect().use(serveStatic(__dirname)).listen(port, function () {
+    console.log(`Server running on http://localhost:${port}`);
+});
