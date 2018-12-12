f962230399e1f99fb804b2446714134f1007b46c
Jenna Murphy <jenna.raye.murphy@gmail.com>
2018-12-12 07:40:18 -0800

    nodework

--- app.js
+++ app.js
@@ -2,8 +2,8 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-    res.sendFile('index.html');
-    root: __;
+    res.sendFile('./index.html');
+    root: __flickrAPI;
 });
 
 app.listen(3000, function () {
